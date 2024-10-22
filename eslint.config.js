5a784f3b8d4e9214b199117a96b1d4ab3d7dd785
Vitor Gabriel <sgvitor@outlook.com.br>
2024-10-21 21:12:08 -0300

    feat: non-empty author when searching by id

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,16 @@
-import globals from "globals";
 import pluginJs from "@eslint/js";
+import globals from "globals";
 
 export default [
     {
         env: { node: true },
         languageOptions: { globals: globals.browser },
-        rules: {},
+        rules: {
+            indent: ["error", 2],
+            "linebreak-style": ["error", "unix"],
+            quotes: ["error", "double"],
+            semi: ["error", "always"],
+        },
     },
     pluginJs.configs.recommended,
 ];
