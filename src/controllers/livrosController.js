825d113805db2de37c6afba899824f08be42c94f
Vitor Gabriel <sgvitor@outlook.com.br>
2024-10-30 00:20:48 -0300

    feat: filtros de busca

--- src/controllers/livrosController.js
+++ src/controllers/livrosController.js
@@ -1,15 +1,26 @@
 import NaoEncontrado from "../erros/NaoEncontrado.js"; // Importa o modelo de livro
-import { livros } from "../models/index.js";
+import ErroRequisicaoIncorreta from "../erros/RequisicaoIncorreta.js";
+import { autores, livros } from "../models/index.js";
 
 class LivroController {
     // Método estático para listar todos os livros
     static listarLivros = async (req, res, next) => {
         try {
-            const livrosResultado = await livros
-                .find()
-                .populate("autor")
-                .exec(); // Popula os autores
-            res.status(200).json(livrosResultado); // Retorna a lista de livros
+            let { limit = 5, pagina = 1 } = req.query; // Obtém os parâmetros da requisição
+            limit = parseInt(limit); // Limite max. de registros por página
+            pagina = parseInt(pagina); // Página atual
+
+            if (!limit || !pagina) {
+                next(new ErroRequisicaoIncorreta()); // Valida os parâmetros
+            } else {
+                const livrosResultado = await livros
+                    .find()
+                    .skip((pagina - 1) * limit)
+                    .limit(Number(limit))
+                    .populate("autor")
+                    .exec(); // Popula os autores
+                res.status(200).json(livrosResultado); // Retorna a lista de livros
+            }
         } catch (erro) {
             next(erro); // Passa o erro para o middleware de tratamento
         }
@@ -72,7 +83,8 @@ class LivroController {
     static excluirLivro = async (req, res, next) => {
         try {
             const id = req.params.id; // Obtém o ID da requisição
-            const livroRemovido = await livros.findByIdAndDelete(id); // Tenta excluir o livro
+            const livroRemovido = await livros.findByIdAndDelete(id);
+            // Tenta excluir o livro
 
             if (!livroRemovido) {
                 next(new NaoEncontrado("Livro não encontrado"));
@@ -87,17 +99,9 @@ class LivroController {
     // Método estático para listar livros por filtro
     static listarLivroPorFiltro = async (req, res, next) => {
         try {
-            const { editora, titulo } = req.query;
+            const busca = await processaBuasca(req.query); // Processa os parâmetros de busca
 
-            const busca = {};
-
-            if (editora) {
-                busca.editora = editora;
-            }
-            if (titulo) {
-                busca.titulo = { $regex: titulo, $options: "i" };
-            }
-            if (!editora && !titulo) {
+            if (Object.keys(busca).length === 0) {
                 next(new NaoEncontrado("Filtro inválido"));
             } else {
                 const livrosResultado = await livros
@@ -110,6 +114,40 @@ class LivroController {
             next(erro); // Passa o erro para o middleware de tratamento
         }
     };
+}
+
+async function processaBuasca(params) {
+    const { editora, titulo, minPaginas, maxPaginas, nomeAutor } = params;
+
+    const busca = {};
+
+    if (editora) {
+        busca.editora = editora;
+    }
+    if (titulo) {
+        busca.titulo = { $regex: titulo, $options: "i" };
+    }
+    if (minPaginas || maxPaginas) {
+        busca.numeroPaginas = {};
+
+        if (minPaginas) {
+            busca.numeroPaginas.$gte = minPaginas;
+        }
+        if (maxPaginas) {
+            busca.numeroPaginas.$lte = maxPaginas;
+        }
+    }
+
+    if (nomeAutor) {
+        const autor = await autores.findOne({ nome: nomeAutor });
+        if (autor) {
+            busca.autor = autor._id;
+        } else {
+            busca.autor = { $exists: false };
+            throw new NaoEncontrado("Autor não encontrado");
+        }
+    }
+    return busca;
 }
 
 export default LivroController; // Exporta a classe LivroController
--- src/middlewares/manipuladorDeErros.js
+++ src/middlewares/manipuladorDeErros.js
@@ -1,14 +1,13 @@
 import mongoose from "mongoose";
 import ErroBase from "../erros/ErroBase.js";
 import ErroValidacao from "../erros/ErroValidacao.js";
-import NaoEncontrado from "../erros/NaoEncontrado.js";
 import ErroRequisicaoIncorreta from "../erros/RequisicaoIncorreta.js";
 export default function manipuladorDeErros(erro, req, res, next) {
     if (erro instanceof mongoose.Error.CastError) {
         new ErroRequisicaoIncorreta().enviarResposta(res);
     } else if (erro instanceof mongoose.Error.ValidationError) {
         new ErroValidacao(erro).enviarResposta(res);
-    } else if (erro instanceof NaoEncontrado) {
+    } else if (erro instanceof ErroBase) {
         erro.enviarResposta(res);
     } else {
         new ErroBase("Erro interno", 500).enviarResposta(res);
